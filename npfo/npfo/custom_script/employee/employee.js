358515dce8b3d472a601cc278b98ea6dfc3fea5e
Kareem <kareem@caratred.com>
2025-03-03 21:10:12 +0530

    child table value get from salary structure

--- npfo/npfo/custom_script/employee/employee.js
+++ npfo/npfo/custom_script/employee/employee.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Employee', {
     refresh(frm) {
-        // Function to update custom_earnings and custom_deductions child table components
+        // Function to update salary component filters in custom_earnings and custom_deductions
         function update_salary_components(table_field) {
             frm.fields_dict[table_field].grid.get_field('salary_component').get_query = function(doc, cdt, cdn) {
                 return {
@@ -11,8 +11,41 @@ frappe.ui.form.on('Employee', {
             };
         }
 
-        // Update salary component filter for both child tables
         update_salary_components('custom_earnings');
         update_salary_components('custom_deductions');
+    },
+
+    custom_salary_structure(frm) {
+        if (frm.doc.custom_salary_structure) {
+            frappe.call({
+                method: 'frappe.client.get',
+                args: {
+                    doctype: 'Salary Structure',
+                    name: frm.doc.custom_salary_structure
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        let salary_structure = r.message;
+
+                        // Clear existing custom_earnings table
+                        frm.clear_table('custom_earnings');
+
+                        // Populate custom_earnings from Salary Structure's earnings table
+                        (salary_structure.earnings || []).forEach(row => {
+                            let child = frm.add_child('custom_earnings');
+                            
+                            // Copy all fields dynamically
+                            Object.assign(child, row);
+                        });
+
+                        // Refresh the child table
+                        frm.refresh_field('custom_earnings');
+                    }
+                }
+            });
+        } else {
+            frm.clear_table('custom_earnings');
+            frm.refresh_field('custom_earnings');
+        }
     }
 });
